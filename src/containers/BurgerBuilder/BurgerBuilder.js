62a3405990976613fe57a689b1b7715d5151e47a
prajwal27 <prajwalsbelagavi.171co229@nitk.edu.in>
2019-08-21 18:42:39 +0530

    burger builder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+
+
+    render() {
+        return (
+            <Auxiliary>
+                <div>Burger</div>
+                <div>Build COntrols</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
