f2caaea406f5f669f9dfbf8914d4eb35e2edd546
prajwal27 <prajwalsbelagavi.171co229@nitk.edu.in>
2019-08-21 18:27:27 +0530

    layout setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
     <div >
-      
+      <Layout>
+        <p>Testing ! </p>
+      </Layout>
     </div>
   );
 }
